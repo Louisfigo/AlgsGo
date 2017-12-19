c30d36ba4fd2a0450f3d56e9fd8ef57bbf958bbc
Louisfigo <83946222@qq.com>
2017-12-19 20:02:12 +0800

    implement all rbt

--- src/net/louis/collection/tree/RedBlackBinarySearchTree.java
+++ src/net/louis/collection/tree/RedBlackBinarySearchTree.java
@@ -5,6 +5,7 @@ import net.louis.CompareUtil;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 import static net.louis.CompareUtil.compare;
@@ -113,6 +114,162 @@ public class RedBlackBinarySearchTree <K,V> {
         return tn;
     }
 
+    public void flipColor2(RBTreeNode<K,V> tn)
+    {
+        tn.color = !tn.color;
+        tn.left.color = !tn.left.color;
+        tn.right.color =  !tn.right.color;
+    }
+
+    private RBTreeNode<K,V> balance(RBTreeNode<K,V> tn) {
+        // assert (h != null);
+
+        if (isRed(tn.right))                      tn = rtleft(tn);
+        if (isRed(tn.left) && isRed(tn.left.left))tn = rtRight(tn);
+        if (isRed(tn.left) && isRed(tn.right))     flipColor2(tn);
+
+        tn.treeSize = size(tn.left) + size(tn.right) + 1;
+        return tn;
+    }
+
+    public boolean isEmpty()
+    {
+        return  root == null;
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
+        // assert check();
+    }
+
+    // delete the key-value pair with the minimum key rooted at h
+    private RBTreeNode<K,V>  deleteMin(RBTreeNode<K,V>  h) {
+        if (h.left == null)
+            return null;
+
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private RBTreeNode<K,V> moveRedLeft(RBTreeNode<K,V> h) {
+        // assert (h != null);
+        // assert isRed(h) && !isRed(h.left) && !isRed(h.left.left);
+
+        flipColor2(h);
+        if (isRed(h.right.left)) {
+            h.right = rtRight(h.right);
+            h = rtleft(h);
+            flipColor2(h);
+        }
+        return h;
+    }
+
+    // Assuming that h is red and both h.right and h.right.left
+    // are black, make h.right or one of its children red.
+    private RBTreeNode<K,V>  moveRedRight(RBTreeNode<K,V>  h) {
+        // assert (h != null);
+        // assert isRed(h) && !isRed(h.right) && !isRed(h.right.left);
+        flipColor2(h);
+        if (isRed(h.left.left)) {
+            h = rtRight(h);
+            flipColor2(h);
+        }
+        return h;
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = deleteMax(root);
+        if (!isEmpty()) root.color = BLACK;
+        // assert check();
+    }
+
+    // delete the key-value pair with the maximum key rooted at h
+    private RBTreeNode<K,V>  deleteMax(RBTreeNode<K,V>  h) {
+        if (isRed(h.left))
+            h = rtRight(h);
+
+        if (h.right == null)
+            return null;
+
+        if (!isRed(h.right) && !isRed(h.right.left))
+            h = moveRedRight(h);
+
+        h.right = deleteMax(h.right);
+
+        return balance(h);
+    }
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+    public void delete(K key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        if (!contains(key)) return;
+
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+        // assert check();
+    }
+
+    // delete the key-value pair with the given key rooted at h
+    private RBTreeNode<K,V>  delete(RBTreeNode<K,V>  h, K key) {
+        // assert get(h, key) != null;
+
+        if (CompareUtil.compare(key,h.key) < 0)  {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        }
+        else {
+            if (isRed(h.left))
+                h = rtRight(h);
+            if (CompareUtil.compare(key,h.key)  == 0 && (h.right == null))
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            if (CompareUtil.compare(key,h.key)  == 0) {
+                RBTreeNode<K,V> x = min(h.right);
+                h.key = x.key;
+                h.value = x.value;
+                // h.val = get(h.right, min(h.right).key);
+                // h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            }
+            else h.right = delete(h.right, key);
+        }
+        return balance(h);
+    }
+
+    public K min() {
+        if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
+        return min(root).key;
+    }
+
+    // the smallest key in subtree rooted at x; null if no such key
+    private RBTreeNode<K,V> min(RBTreeNode<K,V> x) {
+        // assert x != null;
+        if (x.left == null) return x;
+        else                return min(x.left);
+    }
     public V get(K key)
     {
         return  get(root,key);
